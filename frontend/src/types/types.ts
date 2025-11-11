307d505c98ab972d8c122646ef8e7588cc9fe072
Diego Petitto <dmpetitto2004@gmail.com>
2025-11-11 02:40:46 -0300

    Agrego tipos de interfaces y servicios

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,97 +1,41 @@
-export type Evento = {
-    fechaHoraOcurrencia: string,
-    latitudEpicentro: number,
-    latitudHipocentro: number,
-    longitudEpicentro: number,
-    longitudHipocentro: number
-    valorMagnitud: number;
+export interface Evento {
+  fechaHoraOcurrencia: string;
+  latitudEpicentro: number;
+  longitudEpicentro: number;
+  latitudHipocentro: number;
+  longitudHipocentro: number;
+  valorMagnitud: number;
 }
 
-export type DetalleProm =  {
-    eventoSismico: {
-        fechaHoraOcurrencia: string;
-        latitudEpicentro: number;
-        longitudEpicentro: number;
-        latitudHipocentro: number;
-        longitudHipocentro: number;
-        valorMagnitud: number;
-    };
-    datosSismicos: {
-        alcanceSismo: string;
-        clasificacionSismo: string;
-        origenDeGeneracion: string;
-    };
-    seriesTemporales: {
-        codigoEstacion: number;
-        muestrasSismicas: {
-            fechaHoraMuestra: string;
-            detalles: {
-                valor: string;
-                tipoDeDato: {
-                    denominacion: string;
-                    nombreUnidadMedida: string;
-                };
-            }[];
-        }[];
-    }[];
-    cambiosDeEstado: {
-        fechaHoraDesde: string;
-        fechaHoraHasta: string | null;
-        responsableInspeccion: string | null;
-        estado: string;
+interface DatosSismicos {
+    alcanceSismo: string;
+    clasificacionSismo: string;
+    origenDeGeneracion: string;
+}
+
+interface SeriesTemporalesClasificadas {
+    codigoEstacion: number;
+    muestrasSismicas: {
+      fechaHoraMuestra: string;
+      detalles: {
+        valor: string;
+        tipoDeDato: {
+          denominacion: string;
+          nombreUnidadMedida: string;
+        };
+      }[];
     }[];
 }
 
-
-
-
-/* 
-export type DetalleProm = {
-    detalle: Detalle
+interface CambiosEstado {
+    fechaHoraDesde: string;
+    fechaHoraHasta: string | null;
+    responsableInspeccion: string | null;
+    estado: string;
 }
 
-
-export type Detalle = {
-    eventoSismicos: Evento,
-    datosSismicos: DatosSimicos,
-    serieTemporal:SerieTemporal[] 
-    cambioEstado: CambioEstado[]    
+export interface DetalleEvento {
+  datosSismicos: DatosSismicos;
+  seriesTemporales: SeriesTemporalesClasificadas[];
+  cambiosDeEstado: CambiosEstado[];
 }
-
-
-export type DatosSimicos = {
-    alcanceSismo: String,
-    clasificacionSismo: String,
-    origenDeGeneracion: String
-}
-
-export type SerieTemporal = {
-    codigoEstacion: number,
-    muestraSismica: MuestraSismica[]
-}
-
-type MuestraSismica = {
-    fechaHoraMuestra: String, 
-    detalleMuestraSismica: DetalleMuestraSismica[]
-}
-
-type DetalleMuestraSismica = {
-    valor: String,
-    tipoDeDato: TipoDeDato
-
-}
-
-type TipoDeDato = {
-    denominacion: String,
-    nombreUnidadMedida: String
-}
-
-
-export type CambioEstado = {
-    fechaHoraDesde: String,
-    fechaHoraHasta: String,
-    responsableInspeccion: String,
-    estado: String
-}
-
-*/
